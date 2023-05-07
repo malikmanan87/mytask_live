f7a670a74eaf887affe691485e2ccea363256503
min <malikmanan@unisza.edu.my>
2023-05-07 15:06:20 +0800

    add alasan to ambikanak

--- app/Views/forms/ambikanak.php
+++ app/Views/forms/ambikanak.php
@@ -25,16 +25,31 @@
                     echo "<h1>$timestamp<h1>"; ?>
                 </div>
                 <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            Alasan:
+                        </div>
+                        <div class="col-9">
+                            <select class="custom-select" name="alasan">
+                                <option value="1">PRA</option>
+                                <option value="2">KAFA</option>
+                                <option value="3">Klinik</option>
+                                <option value="4">Lain-lain</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <hr>
                     <?php if (!$result) { ?>
-                        <a href="<?= base_url('/saveambikanak'); ?>" class="btn btn-danger btn-lg">Keluar<br><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                        <a href="<?= base_url('/saveambikanak'); ?>" class="btn btn-danger">Keluar <i class="fa fa-arrow-right" aria-hidden="true"></i>
                         </a>
                     <?php } else { ?>
-                        <a href="<?= base_url('/saveambikanak'); ?>" class="btn btn-primary btn-lg">Masuk<br><i class="fa fa-arrow-left" aria-hidden="true"></i>
+                        <a href="<?= base_url('/saveambikanak'); ?>" class="btn btn-primary">Masuk <i class="fa fa-arrow-left" aria-hidden="true"></i>
                             </i>
                         </a>
                     <?php } ?>
-                </div>
 
+                </div>
             </div>
             <div class="card text-center">
                 <div class="card-body">
@@ -45,6 +60,7 @@
                                 <th scope="col">Tarikh</th>
                                 <th scope="col">Masa Keluar</th>
                                 <th scope="col">Masa Masuk</th>
+                                <th scope="col">Alasan</th>
                                 <th scope="col">Hutang</th>
                             </tr>
                         </thead>
@@ -57,10 +73,23 @@
                                     <td><?= $item['masamasuk'] ?></td>
                                     <td>
                                         <?php
+                                        if ($item['alasan'] == 1) {
+                                            echo "PRA";
+                                        } else if ($item['alasan'] == 2) {
+                                            echo "KAFA";
+                                        } else if ($item['alasan'] == 3) {
+                                            echo "Klinik";
+                                        } else {
+                                            echo "Lain-lain";
+                                        }
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
                                         $masakeluar = strtotime($item['masakeluar']);
                                         $masamasuk = strtotime($item['masamasuk']);
                                         $difference = $masamasuk - $masakeluar;
-                                        $difference_minute =  $difference / 60;
+                                        $difference_minute = $difference / 60;
                                         echo intval($difference_minute) . ' minit';
                                         ?>
                                     </td>
